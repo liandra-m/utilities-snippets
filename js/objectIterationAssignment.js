666fd94f488ebfd574a9297ab889646717be2315
Liandra Marques <liandra.mrq@gmail.com>
2023-02-02 08:17:21 -0300

    Rename objectAssignments and add conditional assignment

--- js/objectIterationAssignment.js
+++ js/objectIterationAssignment.js
@@ -1,3 +1,7 @@
+// iteration assignment
 for (let attribute of Object.keys(myObject)) {
-    myObject.attribute = otherObject[attribute];
+    myObj.attribute = otherObject[attribute];
 }
+
+// conditional assignment
+const myObj = { ...(condition && { key: prop }) };
